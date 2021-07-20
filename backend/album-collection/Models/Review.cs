cc51c50b4392887518bd4cf119090a617183304c
joshuabharper7 <84295436+joshuabharper7@users.noreply.github.com>
2021-07-20 14:52:57 -0400

    Review Model

---
+++ backend/album-collection/Models/Review.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace album_collection.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Reviewername { get; set; }
+        public virtual Album Album { get; set; }
+        public int AlbumId { get; set; }
+
+        public Review(int id, int albumId, string reviewername, string content)
+        {
+            Id = id;
+            AlbumId = albumId;
+            Reviewername = reviewername;
+            Content = content;
+        }
+
+        public Review()
+        {
+
+        }
+    }
+}
