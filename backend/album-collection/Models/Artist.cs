9ada8548e60c9fcb579ffe70276962bb3b394a6f
joshuabharper7 <84295436+joshuabharper7@users.noreply.github.com>
2021-07-20 14:31:14 -0400

    Created Artist and Album model

--- backend/album-collection/Models/Artist.cs
+++ backend/album-collection/Models/Artist.cs
@@ -1,8 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
-public class Class1
+namespace album_collection.Models
 {
-	public Class1()
-	{
-	}
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.Upload)]
+        [Display(Name = "Artist Image")]
+        [Required(ErrorMessage = "Please Choose File To Upload")]
+        public string Image { get; set; }
+        public virtual List<Album> Albums { get; set; }
+        public int Age { get; set; }
+        [Display(Name = "Record Label")]
+        public string RecordLabel { get; set; }
+        [Display(Name = "Home Town")]
+        public string HomeTown { get; set; }
+
+        public Artist()
+        {
+
+        }
+
+        public Artist(int id, string name, string image, int age, string recordLabel, string homeTown)
+        {
+            Id = id;
+            Name = name;
+            Image = image;
+            Age = age;
+            RecordLabel = recordLabel;
+            HomeTown = homeTown;
+        }
+
+        public void AddAlbum(Album myAlbum)
+        {
+            Albums.Add(myAlbum);
+        }
+
+    }
 }
