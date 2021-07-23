7b23adc07d6d857856657204f3583a402dfd2178
joshuabharper7 <84295436+joshuabharper7@users.noreply.github.com>
2021-07-23 11:43:41 -0400

    Crud on controllers and added tests

--- backend/album-collection.Tests/SongTests.cs
+++ backend/album-collection.Tests/SongTests.cs
@@ -1,3 +1,4 @@
+using album_collection.Models;
 using System;
 using Xunit;
 
@@ -5,10 +6,38 @@ namespace album_collection.Tests
 {
     public class SongTests
     {
-        [Fact]
-        public void Test1()
-        {
+        Song sut;
 
+        [Fact]
+        public void SongConstructor_Sets_Id_On_Song()
+        {
+            sut = new Song() { Id = 1 };
+
+            Assert.Equal(1, sut.Id);
+        }
+
+        [Fact]
+        public void SongConstructor_Sets_Title_On_Song()
+        {
+            sut = new Song() { Title = "Test" };
+
+            Assert.Equal("Test", sut.Title);
+        }
+
+        [Fact]
+        public void SongConstructor_Sets_Duration_On_Song()
+        {
+            sut = new Song() { Duration = 2 };
+
+            Assert.Equal(2, sut.Duration);
+        }
+
+        [Fact]
+        public void SongConstructor_Sets_AlbumId_On_Song()
+        {
+            sut = new Song() { AlbumId = 3 };
+
+            Assert.Equal(3, sut.AlbumId);
         }
     }
 }
--- backend/album-collection/Controllers/SongController.cs
+++ backend/album-collection/Controllers/SongController.cs
@@ -1,6 +1,7 @@
 ﻿using album_collection.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,14 +20,79 @@ namespace album_collection.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Song> GetSongs()
+        public async Task<ActionResult<IEnumerable<Song>>> GetSongs()
         {
-            return _db.Songs.ToList();
+            return await  _db.Songs.ToListAsync();
         }
         [HttpGet("{id}")]
-        public Song GetSongById(int id)
+        public async Task<ActionResult<Song>> GetSongById(int id)
         {
-            return _db.Songs.Find(id);
+            var song = await _db.Songs.FindAsync(id);
+
+            if(song == null)
+            {
+                return NotFound();
+            }
+
+            return song;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSong(int id, Song song)
+        {
+            if (id != song.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(song).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SongExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Song>> PostSong([FromBody] Song song)
+        {
+            _db.Songs.Add(song);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction("GetSong", new { id = song.Id }, song);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSong(int id)
+        {
+            var song = await _db.Songs.FindAsync(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            _db.Songs.Remove(song);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SongExists(int id)
+        {
+            return _db.Songs.Any(e => e.Id == id);
         }
         
     }
